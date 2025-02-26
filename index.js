93ab1a8f72843404da1d3610192ddadb3c5352f6
Carlos Braga <carlos.braga@gft.com>
2025-02-26 14:56:53 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
 
 // Configurações
-const orsApiKey = process.env.ORS_API_KEY;  
+const orsApiKey = process.env.ORS_API_KEY;
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.CHAT_ID;
 
@@ -18,28 +19,39 @@ async function verificarTrafego() {
 
   try {
     const response = await axios.get(url);
-    const rota = response.data.routes[0];
+    const rota = response.data.features[0].properties;
     const duracao = rota.summary.duration / 60; // Converter segundos para minutos
 
-    if (duracao > 17) { // Se o tempo de viagem for maior que o tempo normal em min, alertar
-      await enviarAlerta(`⚠️ Atraso detectado! Tempo estimado: ${Math.round(duracao)} min.`);
+    if (duracao > 16) {
+      // Se o tempo de viagem for maior que o tempo normal em min, alertar
+      await enviarAlerta(
+        `⚠️ Atraso detectado! Tempo estimado: ${Math.round(duracao)} min.`
+      );
     } else {
       console.log("✅ Tráfego dentro do normal.");
     }
   } catch (error) {
-    console.error("❌ Erro ao verificar o tráfego:", error.response?.data || error.message);
+    console.error(
+      "❌ Erro ao verificar o tráfego:",
+      error.response?.data || error.message
+    );
   }
 }
 
 // Função para enviar alerta no Telegram
 async function enviarAlerta(mensagem) {
-  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(mensagem)}`;
+  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(
+    mensagem
+  )}`;
 
   try {
     await axios.get(url);
     console.log("✅ Alerta enviado com sucesso.");
   } catch (error) {
-    console.error("❌ Erro ao enviar alerta para o Telegram:", error.response?.data || error.message);
+    console.error(
+      "❌ Erro ao enviar alerta para o Telegram:",
+      error.response?.data || error.message
+    );
   }
 }
 
